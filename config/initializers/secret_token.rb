6e61c41e3a5be96a4adf2109bf61ec18a8a67b7d
suVene <suvene@zeromemory.info>
2012-03-14 03:41:34 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror2pm::Application.config.secret_token = '456022ad4aadd6822267454d43ee1aa5c3d550b947c54a90fcea3d652173ae4da95dba9fbb5b4ff63f93d0383ff3f27ec483b6fe50fe9967f83129aab4bcb455'
